a128f89facf9df9d5f893470071e76a6820a4d50
Three6ty1 <wk.zhang02@gmail.com>
2024-07-14 03:12:04 +1000

    Linting

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,6 +1,6 @@
 declare module '*.svg' {
     import React = require('react');
-    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    export const SvgComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
     const src: string;
     export default src;
   }
