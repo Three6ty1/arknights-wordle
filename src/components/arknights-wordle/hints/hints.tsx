239b6ae2b2f0774cb3dfd252cae92f8896ed6bb9
Three6ty1 <wk.zhang02@gmail.com>
2024-08-17 22:30:55 +1000

    Removed line

--- src/components/arknights-wordle/hints/hints.tsx
+++ src/components/arknights-wordle/hints/hints.tsx
@@ -29,7 +29,7 @@ export default function Hints() {
               <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h14"/>
             </svg>
           </div>
-          <div tabIndex={0} className="dropdown-content dropdown-bottom bg-base-100 rounded-md z-[100] w-fit mt-1 p-1 shadow-sm shadow-neutral-content space-y-1">
+          <div tabIndex={0} className="dropdown-content bg-base-100 rounded-md z-[100] w-fit mt-1 p-1 shadow-sm shadow-neutral-content space-y-1">
             <HintHelp />
             <Statistics />
             <Theme />
