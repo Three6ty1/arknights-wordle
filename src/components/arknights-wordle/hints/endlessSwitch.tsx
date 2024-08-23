21f765d258b148c966614494090f1ec9ca1d29c4
Three6ty1 <wk.zhang02@gmail.com>
2024-08-23 23:42:49 +1000

    Removed disabled endless. Added countdown. Updated version log

--- src/components/arknights-wordle/hints/endlessSwitch.tsx
+++ src/components/arknights-wordle/hints/endlessSwitch.tsx
@@ -4,26 +4,28 @@ import { GameModeContext, ThemeContext } from "~/pages/index";
 export default function EndlessSwitch() {
   const {isNormalMode, setIsNormalMode, playing} = React.useContext(GameModeContext)
   const {highContrast} = React.useContext(ThemeContext)
-  const [animate, setAnimate] = React.useState(false)
+  // const [animate, setAnimate] = React.useState(false)
 
   const buttonClass = highContrast ? "btn-info" : "btn-success"
 
   return (
+    /*
     <div
     className={`indicator-item tooltip m-2 before:whitespace-pre-wrap ${animate ? "animate-shake" : ""}`}
     data-tip={playing ? "Endless Mode is\nlocked until the\nDaily Game is\nfinished" : "Endless Mode"}
     onClick={() => {if (playing) {setAnimate(true); setTimeout(() =>setAnimate(false), 200);}}}
     >
-      <button 
-        className={"btn indicator-item tooltip flex items-center " + (!isNormalMode ? buttonClass : "")}
-        data-tip="Endless Mode"
-        disabled={playing}
-        onClick={() => setIsNormalMode(!isNormalMode)}
-      >
-        <svg className={"w-6 h-6 " + (!isNormalMode && "text-white animate-slow-spin")} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/>
-        </svg>
-      </button> 
     </div>
+    */
+    <button 
+      className={"btn indicator-item tooltip flex items-center m-2 " + (!isNormalMode ? buttonClass : "")}
+      data-tip="Endless Mode"
+      // disabled={playing}
+      onClick={() => setIsNormalMode(!isNormalMode)}
+    >
+      <svg className={"w-6 h-6 " + (!isNormalMode && "text-white animate-slow-spin")} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/>
+      </svg>
+    </button> 
   )
 }
